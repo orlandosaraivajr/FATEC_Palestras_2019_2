4a5f5b0bb0c73ef40154d48d5d0ebe771f2d6f5f
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-09-19 20:44:15 -0300

    Criação do pacote domínio e mudança no pacote Resource

--- src/main/java/com/fatecararas/palestras/resources/CategoriaResource.java
+++ src/main/java/com/fatecararas/palestras/resources/CategoriaResource.java
@@ -1,14 +1,24 @@
 package com.fatecararas.palestras.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fatecararas.palestras.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String lista() {
-		return "Rest está funcionando";
+	public List<Categoria> lista() {
+		Categoria cat1 = new Categoria(1, "Desenvolvimento");
+		Categoria cat2 = new Categoria(2, "Segurança da Informação");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
