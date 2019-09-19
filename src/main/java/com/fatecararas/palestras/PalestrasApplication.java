25770717887a070fc9bc08edb21fa7accb748f30
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-09-19 20:21:24 -0300

    Primeiro commit

---
+++ src/main/java/com/fatecararas/palestras/PalestrasApplication.java
@@ -0,0 +1,13 @@
+package com.fatecararas.palestras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalestrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalestrasApplication.class, args);
+	}
+
+}
