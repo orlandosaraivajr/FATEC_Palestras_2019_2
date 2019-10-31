e04b219c9c939658c86a3e2f96b40d11648a8a58
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-10-31 20:08:41 -0300

    Estratégias quanto a herança e gerar tabelas no banco de dados

--- src/main/java/com/fatecararas/palestras/domain/Pagamento.java
+++ src/main/java/com/fatecararas/palestras/domain/Pagamento.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
@@ -11,6 +13,8 @@ import javax.persistence.OneToOne;
 import com.fatecararas.palestras.domain.enums.EstadoPagamento;
 
 @Entity
+// @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Pagamento implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
