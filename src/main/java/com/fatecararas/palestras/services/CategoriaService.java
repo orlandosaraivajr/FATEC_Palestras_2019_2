56ca89f190dfa3b5bde672721098c1de2b3fb6e7
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-11-07 22:53:00 -0200

    Adicinado o método PUT ao endpoint /categorias

--- src/main/java/com/fatecararas/palestras/services/CategoriaService.java
+++ src/main/java/com/fatecararas/palestras/services/CategoriaService.java
@@ -23,4 +23,10 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+	
+	public Categoria update(Categoria obj) {
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
 }
