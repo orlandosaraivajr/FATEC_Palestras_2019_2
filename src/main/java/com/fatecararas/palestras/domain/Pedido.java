bf692847ff7fbf62426277ef5b061522aab2e350
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-11-07 22:20:50 -0200

    Ajuste no formato para serializar a data

--- src/main/java/com/fatecararas/palestras/domain/Pedido.java
+++ src/main/java/com/fatecararas/palestras/domain/Pedido.java
@@ -15,6 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -23,6 +24,7 @@ public class Pedido implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 	
 	@JsonIgnore
